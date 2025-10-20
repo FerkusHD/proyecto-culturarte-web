13787d7d5f057661766183787fcec3c5998f4dfb
Fabricio <118315951+FerkusHD@users.noreply.github.com>
2025-10-20 01:50:59 -0300

    build de docker
    funcionando pero problema de lentitud al cargar primera vez

--- src/main/java/com/culturarte/web/PryectoCulturarteWebApplication.java
+++ src/main/java/com/culturarte/web/PryectoCulturarteWebApplication.java
@@ -3,12 +3,19 @@ package com.culturarte.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.culturarte.logica.clases"})
 @ComponentScan(basePackages = {"com.culturarte.web", "com.culturarte.logica"})
-public class PryectoCulturarteWebApplication {
+public class PryectoCulturarteWebApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PryectoCulturarteWebApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PryectoCulturarteWebApplication.class, args);
