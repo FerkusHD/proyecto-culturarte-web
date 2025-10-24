0904715e17763a3c2488f6ae620c8fdfb7d1cb11
Fabricio <118315951+FerkusHD@users.noreply.github.com>
2025-10-24 20:38:27 -0300

    estructura soap

--- core-business/src/main/java/com/culturarte/logica/AppApplication.java
+++ core-business/src/main/java/com/culturarte/logica/AppApplication.java
@@ -2,10 +2,6 @@
 
 package com.culturarte.logica;
 
-/**
- * Placeholder class to keep package directory compiling without GUI entry point.
- * Core module should not contain a main method nor GUI dependencies.
- */
 public final class AppApplication {
     private AppApplication() {}
 }
